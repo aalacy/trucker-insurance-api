6db2762a3ccf307a8ddbcbc4d561b8be72a3c3da
aalacy <aaronlacywhite387@gmail.com>
2020-01-28 14:16:02 -0800

    fix home

--- utils/company-snapshot.js
+++ utils/company-snapshot.js
@@ -22,7 +22,6 @@ module.exports = {
               reject("empty response from the server");
               return;
             }
-            console.log('snapshot:'+_this.parseHTML(body));
             resolve(_this.parseHTML(body));
         }
       );
