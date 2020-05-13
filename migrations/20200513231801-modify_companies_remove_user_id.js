e754e8515a5769e90d84094ace82d31bbe50dc60
aalacy <aaronlacywhite387@gmail.com>
2020-05-13 16:25:27 -0700

    create migration remove foreign key contraint in user_id in Companies table

---
+++ migrations/20200513231801-modify_companies_remove_user_id.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('Companies', 'user_id', {
+        type : Sequelize.INTEGER,
+        allowNull : true,
+      });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('Companies', 'user_id');
+  }
+};
